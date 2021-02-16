f20a6e482a28a0d70f785337af3ca048d211d7bf
cezaris13 <pijus.cezar13@gmail.com>
2021-02-16 12:47:28 +0200

    other domination problems

--- 12-knights/main.cpp
+++ 12-knights/main.cpp
@@ -1,41 +1,20 @@
 #include <bits/stdc++.h>
 #define SIZE 8
 using namespace std;
+int movesx[8] = {2,1,-1,-2,-2,-1,1,2};
+int movesy[8] = {1,2,2,1,-1,-2,-2,-1};
 bool checkUnused(int lenta[SIZE][SIZE]){
     bool vis[SIZE][SIZE]={{0}};
     for(int i=0;i<SIZE;i++){
         for(int j=0;j<SIZE;j++){
             if(lenta[i][j]==1){
-                for(int k=0;k<SIZE;k++){
-                    vis[k][j]=1;
-                    vis[i][k]=1;
-                }
-                int tempx=i,tempy=j;
-                while(tempx<SIZE && tempy>=0){
-                    vis[tempx][tempy]=1;
-                    tempy--;
-                    tempx++;
-                }
-                tempx=i;
-                tempy=j;
-                while(tempx<SIZE && tempy<SIZE){
-                    vis[tempx][tempy]=1;
-                    tempy++;
-                    tempx++;
-                }
-                tempx=i;
-                tempy=j;
-                while(tempx>=0 && tempy>=0){
-                    vis[tempx][tempy]=1;
-                    tempy--;
-                    tempx--;
-                }
-                tempx=i;
-                tempy=j;
-                while(tempx>=0 && tempy<SIZE){
-                    vis[tempx][tempy]=1;
-                    tempy++;
-                    tempx--;
+                vis[i][j]=1;
+                for(int k=0;k<8;k++){
+                    int newx=i+movesx[k];
+                    int newy=j+movesy[k];
+                    if(newx>=0 && newx<SIZE && newy>=0 && newy<SIZE){
+                        vis[newx][newy]=1;
+                    }
                 }
             }
         }
@@ -49,7 +28,7 @@ bool checkUnused(int lenta[SIZE][SIZE]){
     }
     return 1;
 }
-int countQueens(int lenta[SIZE][SIZE]){
+int countKnights(int lenta[SIZE][SIZE]){
     int count=0;
     for(int i=0;i<SIZE;i++){
         for(int j=0;j<SIZE;j++){
@@ -61,7 +40,14 @@ int countQueens(int lenta[SIZE][SIZE]){
     return count;
 }
 bool solve(int lenta[SIZE][SIZE],vector<pair<int,pair<int,int> > > probs,int index){
-    if(countQueens(lenta)==5){
+    if(countKnights(lenta)==12){
+  for(int i=0;i<SIZE;i++){
+                for(int j=0;j<SIZE;j++){
+                    printf("%2.1d",lenta[i][j]);
+                }
+                printf("\n");
+            }
+            printf("\n");
         if(checkUnused(lenta)){
             // for(int i=0;i<SIZE;i++){
             //     for(int j=0;j<SIZE;j++){
@@ -89,35 +75,14 @@ bool solve(int lenta[SIZE][SIZE],vector<pair<int,pair<int,int> > > probs,int ind
     return 0;
 }
 int Calc(int x,int y){
-    int calc=15;
-    int tempx=x,tempy=y;
-    while(tempx<SIZE && tempy>=0){
-        calc++;
-        tempy--;
-        tempx++;
-    }
-    tempx=x;
-    tempy=y;
-    while(tempx<SIZE && tempy<SIZE){
-        calc++;
-        tempy++;
-        tempx++;
-    }
-    tempx=x;
-    tempy=y;
-    while(tempx>=0 && tempy>=0){
-        calc++;
-        tempy--;
-        tempx--;
-    }
-    tempx=x;
-    tempy=y;
-    while(tempx>=0 && tempy<SIZE){
-        calc++;
-        tempy++;
-        tempx--;
-    }
-    calc-=4;
+     int calc=0;
+     for(int i=0;i<8;i++){
+         int newx=x+movesx[i];
+         int newy=y+movesy[i];
+         if(newx>=0 && newx<SIZE && newy>=0 && newy<SIZE){
+             calc++;
+         }
+     }
     return calc;
 }
 int main() {
@@ -127,10 +92,15 @@ int main() {
     vector<pair<int,pair<int,int> > > probs;
     for(int i=0;i<SIZE;i++){
         for(int j=0;j<SIZE;j++){
+            // printf("%2.1d",Calc(i,j));
             probs.push_back(make_pair(Calc(i,j),make_pair(i,j)));
         }
+        // printf("\n");
     }
     sort(probs.rbegin(),probs.rend());
+    // for(int i=0;i<probs.size();i++){
+    //     printf("%2.1d. %d (%d %d)\n",i+1,probs[i].first,probs[i].second.first,probs[i].second.second);
+    // }
     if(solve(lenta,probs,0)){
         for(int i=0;i<SIZE;i++){
             for(int j=0;j<SIZE;j++){
