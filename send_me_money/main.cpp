83236ea953cc24b02c1990626bf2284970a07828
cezaris13 <pijus.cezar13@gmail.com>
2021-02-19 08:58:21 +0200

    recursive approach of send-more-money puzzle

--- send_me_money/main.cpp
+++ send_me_money/main.cpp
@@ -1,40 +1,33 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
-
-int main() {
-    for(int s=0;s<=9;s++){
-        for(int e=0;e<=9;e++){
-            for(int n=0;n<=9;n++){
-                for(int d=0;d<=9;d++){
-                    for(int m=1;m<=9;m++){
-                        for(int o=0;o<=9;o++){
-                            for(int r=0;r<=9;r++){
-                                for(int y=0;y<=9;y++){
-                                    if(1000*s+100*e+10*n+d+1000*m+100*o+10*r+e==10000*m+1000*o+100*n+10*e+y){
-                                        int arr[8]={s,e,n,d,m,o,r,y};
-                                        bool tru=1;
-                                        for(int i=0;i<7;i++){
-                                            for(int j=i+1;j<8;j++){
-                                                if(arr[i]==arr[j]){
-                                                    tru=0;
-                                                    j=8;
-                                                    i=7;
-                                                }
-                                            }
-                                        }
-                                        if(tru){
-                                            cout<<s<<e<<n<<d<<endl<<m<<o<<r<<e<<endl<<m<<o<<n<<e<<y<<endl<<endl;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+bool solve(vector<int> &total_ans,bool taken[10]){
+    if(total_ans.size()==8){
+        if(1000*total_ans[0]+100*total_ans[1]+10*total_ans[2]+total_ans[3]+1000*total_ans[4]+100*total_ans[5]+10*total_ans[6]+total_ans[1]==10000*total_ans[4]+1000*total_ans[5]+100*total_ans[2]+10*total_ans[1]+total_ans[7]){
+            if(total_ans[4]!=0){
+                return 1;
             }
         }
+        return 0;
     }
-
+    for(int i=0;i<10;i++){
+        if(!taken[i]){
+            total_ans.push_back(i);
+            taken[i]=1;
+            if(solve(total_ans,taken)){
+                return 1;
+            }
+            total_ans.pop_back();
+            taken[i]=0;
+        }
+    }
+    return 0;
+}
+int main() {
+    vector<int>total_ans;
+    bool visited[10]={0};
+    if(solve(total_ans,visited)){
+        printf(" %d%d%d%d\n %d%d%d%d\n%d%d%d%d%d",total_ans[0],total_ans[1],total_ans[2],total_ans[3],total_ans[4],total_ans[5],total_ans[6],total_ans[1],total_ans[4],total_ans[5],total_ans[2],total_ans[1],total_ans[7]);
+    }
     return 0;
 }
