c98bdffd0120b63ae893862d18931fc30b5d81ed
cezaris13 <pijus.cezar13@gmail.com>
2021-04-05 15:05:41 +0300

    renamed some variables

--- bank_simulation/main.c
+++ bank_simulation/main.c
@@ -3,73 +3,73 @@
 #include <time.h>
 #include "queue.h"
 #include "big_int.h"
-struct WorkersType{
-    int totalWorkers;
+struct ConsultantType{
+    int totalConsultants;
     int timeForOneClient;
 };
-typedef struct WorkersType Workers;
-void readData(Workers *experiencedWorkers,Workers *newWorkers,int *prob){
+typedef struct ConsultantType Consultant;
+void readData(Consultant *experiencedConsultant,Consultant *newConsultant,int *prob){
     FILE *readData=fopen("duota.txt", "r");
-    fscanf(readData, "%d%*[^\n]",&((*experiencedWorkers).totalWorkers));
-    fscanf(readData, "%d%*[^\n]",&((*experiencedWorkers).timeForOneClient));
-    fscanf(readData, "%d%*[^\n]",&((*newWorkers).totalWorkers));
-    fscanf(readData, "%d%*[^\n]",&((*newWorkers).timeForOneClient));
+    fscanf(readData, "%d%*[^\n]",&(experiencedConsultant->totalConsultants));
+    fscanf(readData, "%d%*[^\n]",&(experiencedConsultant->timeForOneClient));
+    fscanf(readData, "%d%*[^\n]",&(newConsultant->totalConsultants));
+    fscanf(readData, "%d%*[^\n]",&(newConsultant->timeForOneClient));
     fscanf(readData, "%d%*[^\n]",prob);
     fclose(readData);
 }
-void checkWork(Workers type,int prob,int specificTime,int seed){
+void checkWork(Consultant consultant,int prob,int specificTime,int seed){
+    //data for statistics
     int queSize=0;
+    int *clientsWaitTime=calloc(specificTime, sizeof(int));
+    int clients=0;
+    double *totalOneConsultantClients=calloc(consultant.totalConsultants, sizeof(double));
+
     srand(seed);
     Queue listOfCustomers;
     initialize(&listOfCustomers);
-    int *workerList=calloc(type.totalWorkers, sizeof(int));
-    double *totalWorkerClients=calloc(type.totalWorkers, sizeof(double));
-    double totalClients=0;
-    int *clientsWaitTime=calloc(specificTime, sizeof(int));
-    int clients=0;
+    int *consultantList=calloc(consultant.totalConsultants, sizeof(int));
     for(int i=1;i<=specificTime;i++){
-        for(int j=0;j<type.totalWorkers;j++){
-            if(workerList[j]>0){
-                workerList[j]--;
+        for(int j=0;j<consultant.totalConsultants;j++){
+            if(consultantList[j]>0){
+                consultantList[j]--;
             }
         }
         if(rand()%100<prob){
             if(isEmpty(&listOfCustomers)){
-                int j,in=0;
-                for(j=0;j<type.totalWorkers;j++){
-                    if(workerList[j]==0){
+                int in=0;
+                for(int j=0;j<consultant.totalConsultants;j++){
+                    if(consultantList[j]==0){
                         clients++;//person did not wait
-                        workerList[j]=type.timeForOneClient;
-                        totalWorkerClients[j]++;
+                        consultantList[j]=consultant.timeForOneClient;
+                        totalOneConsultantClients[j]++;
                         in=1;
                         break;
                     }
                 }
                 if(!in){
-                    enqueue(&listOfCustomers, i);
+                    if(!isFull(&listOfCustomers)){
+                        enqueue(&listOfCustomers, i);
+                    }
                 }
             }
             else{
-                enqueue(&listOfCustomers, i);
+                if(!isFull(&listOfCustomers)){
+                    enqueue(&listOfCustomers, i);
+                }
             }
         }
-        for(int j=0;j<type.totalWorkers;j++){
-            if(workerList[j]==0&&(listOfCustomers.count)>0){
-                int clientQueTime=getFirst(&listOfCustomers);
-                clientsWaitTime[clients]=i-clientQueTime;
-                clients++;
-                workerList[j]=type.timeForOneClient;
-                totalWorkerClients[j]++;
-                int queue_int=0;
+        for(int j=0;j<consultant.totalConsultants;j++){
+            if(consultantList[j]==0&&(listOfCustomers.count)>0){
+                clientsWaitTime[clients]=i-getFirst(&listOfCustomers);//time Spent in queue
+                clients++;//how many cliens came to The concultants
+                consultantList[j]=consultant.timeForOneClient;
+                totalOneConsultantClients[j]++;
                 dequeue(&listOfCustomers);
             }
         }
        queSize=(queSize<(listOfCustomers.count)?(listOfCustomers.count):queSize);
     }
     /* statistics */
-    for(int i=0;i<type.totalWorkers;i++){
-        totalClients+=totalWorkerClients[i];
-    }
     double sum=0;
     int maxTime=0;
     for(int i=0;i<clients;i++){
@@ -77,34 +77,34 @@ void checkWork(Workers type,int prob,int specificTime,int seed){
         maxTime=(clientsWaitTime[i]<maxTime?maxTime:clientsWaitTime[i]);
     }
     if(clients!=0){
-        for(int i =0;i<type.totalWorkers;i++){
-            printf("worker[%d] busy: %f%%\n",i,(totalWorkerClients[i]/totalClients)*100);
+        for(int i =0;i<consultant.totalConsultants;i++){
+            printf("%2.0d Consultant was busy with %f%% of clients\n",i+1,(totalOneConsultantClients[i]/clients)*100);
         }
         printf("avg wait: %f\nmax wait: %d\n",sum/clients,maxTime);
+        printf("max queue size: %d\n",queSize);
     }
     else{
         printf("no one came\n");
     }
-    free(workerList);
+    free(consultantList);
     free(clientsWaitTime);
-    free(totalWorkerClients);
+    free(totalOneConsultantClients);
     destroy(&listOfCustomers);
-    printf("max queue size: %d\n",queSize);
 }
 int main(){
     int prob,specificTime;
     int seed;
-    seed=10;
+    /* seed=10; */
     printf("iveskite norima seed'a\n");
     scanf("%d",&seed);
-    Workers newWorkers,experiencedWorkers;
-    readData(&experiencedWorkers,&newWorkers,&prob);
+    Consultant newConsultant,experiencedConsultant;
+    readData(&experiencedConsultant,&newConsultant,&prob);
     printf("choose specific time to see the results\n");
     scanf("%d",&specificTime);
-    specificTime=100;
-    printf("\nexperienced workers\n");
-    checkWork(experiencedWorkers, prob,specificTime,seed);
-    printf("\nnew workers\n");
-    checkWork(newWorkers, prob,specificTime,seed);
+    /* specificTime=100; */
+    printf("\nexperienced Consultant\n");
+    checkWork(experiencedConsultant, prob,specificTime,seed);
+    printf("\nnew Consultant\n");
+    checkWork(newConsultant, prob,specificTime,seed);
     return 0;
 }
